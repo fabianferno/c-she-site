94595f85ab680a8c5d6e259c20db476b5b6c1e09
fabianferno <fabianferno@gmail.com>
2022-02-27 20:35:39 +0530

    Create NotFound Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { Fragment } from "react";
-import { Link } from "react-router-dom";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
+
+import { Disclosure } from "@headlessui/react";
+import { MoonIcon, SunIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
+
+import { Link, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
-import { Routes, Route } from "react-router-dom";
+import NotFound from "./pages/NotFound/Index";
 
 const user = {
   name: "Tom Cook",
@@ -75,50 +77,9 @@ function App() {
                           type="button"
                           className="bg-gray-800 p-1 text-gray-400 rounded-full hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                         >
-                          <span className="sr-only">View notifications</span>
-                          <BellIcon className="h-6 w-6" aria-hidden="true" />
+                          <span className="sr-only">Dark Mode</span>
+                          <MoonIcon className="h-6 w-6" aria-hidden="true" />
                         </button>
-
-                        {/* Profile dropdown */}
-                        <Menu as="div" className="ml-3 relative">
-                          <div>
-                            <Menu.Button className="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                              <span className="sr-only">Open user menu</span>
-                              <img
-                                className="h-8 w-8 rounded-full"
-                                src={user.imageUrl}
-                                alt=""
-                              />
-                            </Menu.Button>
-                          </div>
-                          <Transition
-                            as={Fragment}
-                            enter="transition ease-out duration-100"
-                            enterFrom="transform opacity-0 scale-95"
-                            enterTo="transform opacity-100 scale-100"
-                            leave="transition ease-in duration-75"
-                            leaveFrom="transform opacity-100 scale-100"
-                            leaveTo="transform opacity-0 scale-95"
-                          >
-                            <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                              {userNavigation.map((item) => (
-                                <Menu.Item key={item.name}>
-                                  {({ active }) => (
-                                    <a
-                                      href={item.href}
-                                      className={classNames(
-                                        active ? "bg-gray-100" : "",
-                                        "block px-4 py-2 text-sm text-gray-700"
-                                      )}
-                                    >
-                                      {item.name}
-                                    </a>
-                                  )}
-                                </Menu.Item>
-                              ))}
-                            </Menu.Items>
-                          </Transition>
-                        </Menu>
                       </div>
                     </div>
                     <div className="-mr-2 flex md:hidden">
@@ -140,24 +101,6 @@ function App() {
               </div>
 
               <Disclosure.Panel className="border-b border-gray-700 md:hidden">
-                <div className="px-2 py-3 space-y-1 sm:px-3">
-                  {navigation.map((item) => (
-                    <Link key={item.name} to={item.to}>
-                      <Disclosure.Button
-                        as="a"
-                        className={classNames(
-                          item.current
-                            ? "bg-gray-900 text-white"
-                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                          "block px-3 py-2 rounded-md text-base font-medium"
-                        )}
-                        aria-current={item.current ? "page" : undefined}
-                      >
-                        {item.name}
-                      </Disclosure.Button>
-                    </Link>
-                  ))}
-                </div>
                 <div className="pt-4 pb-3 border-t border-gray-700">
                   <div className="flex items-center px-5">
                     <div className="flex-shrink-0">
@@ -179,21 +122,9 @@ function App() {
                       type="button"
                       className="ml-auto bg-gray-800 flex-shrink-0 p-1 text-gray-400 rounded-full hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                     >
-                      <span className="sr-only">View notifications</span>
-                      <BellIcon className="h-6 w-6" aria-hidden="true" />
+                      <span className="sr-only">Dark Mode</span>
+                      <MoonIcon className="h-6 w-6" aria-hidden="true" />
                     </button>
-                  </div>
-                  <div className="mt-3 px-2 space-y-1">
-                    {userNavigation.map((item) => (
-                      <Disclosure.Button
-                        key={item.name}
-                        as="a"
-                        href={item.href}
-                        className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700"
-                      >
-                        {item.name}
-                      </Disclosure.Button>
-                    ))}
                   </div>
                 </div>
               </Disclosure.Panel>
@@ -208,6 +139,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="about" element={<About />} />
             <Route path="contact" element={<Contact />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
       </main>
